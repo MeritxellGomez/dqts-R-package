7d52aa0ba1fc503851467f801c8b5c7468327052
mgomez <mgomez@tekniker.es>
2020-05-28 17:16:39 +0200

    a function to treat windows is created

---
+++ R/movingwindows.R
@@ -0,0 +1,28 @@
+
+movingwindow <- function(data, cte=TRUE, fixed=TRUE, nint=3){
+
+  n <- nrow(data)
+  int <- round(n/nint)
+
+  if(cte & fixed){
+
+    for(i in 1:nint){
+
+
+    }
+
+    data[['interval']] <- a
+
+  }else if(cte){
+
+    print(2)
+
+  }else{
+
+    print(3)
+
+  }
+
+}
+
+
