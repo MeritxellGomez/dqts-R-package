ed0f7f3c2b34a13737920f0699d66e8a5c853738
mgomez <mgomez@tekniker.es>
2020-05-29 16:56:00 +0200

    movDQplot is added. the plot shows the metrics evolution

--- R/visualization.R
+++ R/visualization.R
@@ -1,10 +1,12 @@
-qualityplot <- function(dataquality, totalquality = FALSE){
-  
-  if(totalquality==FALSE){dataquality<-dataquality[-length(dataquality)]}
 
-  df.quality <- data.frame(id=seq(1,length(dataquality)),
-                           metric = names(dataquality),
-                           score = unlist(dataquality), stringsAsFactors = FALSE)
+#barplot para salida de DQ cuando no hay ventanas. Calidad TOTAL
+qualitybarplot <- function(DQ, totalquality = FALSE){
+  browser()
+  if(totalquality==FALSE){DQ<-DQ[-length(DQ)]}
+
+  df.quality <- data.frame(id=seq(1,length(DQ)),
+                           metric = names(DQ),
+                           score = unlist(DQ), stringsAsFactors = FALSE)
 
   library(ggplot2)
   g <- ggplot(data=df.quality, aes(x=metric, y=score)) +
@@ -15,3 +17,18 @@ qualityplot <- function(dataquality, totalquality = FALSE){
 
   return(g)
 }
+
+#gráfico cuando hay ventanas en DQ
+movDQplot <- function(movingDQ, totalquality=FALSE){
+
+  library(reshape2)
+
+  d <- melt(movingDQ, id.vars=c("InitialDate", "FinalDate"))
+
+  ggplot(d, aes(FinalDate,value, col=variable)) +
+    geom_line() +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90))
+}
+
+
