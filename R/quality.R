98e7006d3503571ab448d47c3bfb66d80a58022a
mgomez <mgomez@tekniker.es>
2020-07-06 16:59:22 +0200

    excepcions added in DQ function for columnDate, dataref and ranges not available

--- R/quality.R
+++ R/quality.R
@@ -3,38 +3,43 @@
 #' Takes a data set and the conditions to compute the data quality and returns the values of all metrics
 #' @param data A data frame to be analyzed
 #' @return A data frame containing the values of all quality metrics in columns
+#' @importFrom magrittr %>%
 #' @export
-DQ <- function(data, columnDate, maxdif, dataref, ranges = NULL, weights = NULL, windows=FALSE, cte=TRUE, fixed=TRUE, nint=3, by=10){
+DQ <- function(data, columnDate = NULL, maxdif = NULL, dataref = NULL, ranges = NULL, weights = NULL, windows=FALSE, cte=TRUE, fixed=TRUE, nint=3, by=10){
 
   if(is.null(columnDate)){
+    n <- length(which(sapply(data, class)=='Date'))
+    if(n==1){
+      columnDate <- which(sapply(data, class)=='Date')
+    }else if(n==0){
+      stop('No variable with Date format')
+    }else{
+      stop('More than one variable with Date format')
+    }
+  }
 
-    #buscar la forma de que detecte el fomato de fecha y le asigne la columna a columnDate
+  if(is.null(dataref)){
+      warning('Reference data frame should be given. The original data has been taken as reference data')
+      dataref <- data
+  }
 
+  if(is.null(ranges)){
+    warning('Range data frame should be given. The maximum and minimum values from the original data have been taken as range data')
+    ranges <- generateRangeData(data)
   }
 
   if(is.null(maxdif)){ #if maxdif is null, then the most frequent value is assigned
-
     diffs <- diff(data[,columnDate])
-
     uniq <- unique(diffs)
-
     maxdif <- uniq[which.max(tabulate(match(diffs, uniq)))]
   }
-
 
   if(isFALSE(windows)){
     myquality <- quality(data, columnDate, maxdif, dataref, ranges, weights)
   }else{
-
-  source(here::here('R', 'movingwindows.R'))
-  library(dplyr)
-
     minidf <- data %>% nrow() %>% wdwind(., cte, fixed, nint, by) %>% segwdw(data,.)
-
     myquality <- do.call(rbind,lapply(minidf, function(x) quality(x, columnDate, maxdif, dataref, ranges, weights)))
-
   }
-
 
   return(myquality)
 }
