07ebbac0260a7b198e4da65f40a0cdf34ebe4c50
mgomez <mgomez@tekniker.es>
2020-07-06 13:07:37 +0200

    handle_DQ function is created. metric name is an argument to choose which handling function have to be executed

--- R/HandlingLowQuality.R
+++ R/HandlingLowQuality.R
@@ -1,9 +1,72 @@
 ###crear una función que lea los datos y la métrica a corregir y aplique en cada caso la función que deba
 
-# HLQ <- function(data, metric){
-#
-#
-# }
+handle_DQ <- function(data, metric, var_time_name){
+
+  if(metric == "Completeness"){HLCompleteness(data)}
+  else if(metric == "TimeUniqueness"){HLTimeUniqueness(data, var_time_name)}
+  else if(metric == 'Range'){HLRange(data)}
+  else if(metric == 'Consistency'){HLConsistency(data)}
+  else if(metric == 'Typicality'){HLTypicality(data)}
+  else if(metric == 'Moderation'){HLModeration(data)}
+  else if(metric == "Timeliness"){HLTimeliness(data)}
+  else if(metric == "Conformity"){HLConformity(data)}
+  else(return(paste('Incorrect metric name')))
+
+}
+
+
+# Handling Low Completeness -----------------------------------------------
+
+HLCompleteness <- function(data){
+
+  #identificar la cantidad de NA's
+
+  #imputar los NA's
+
+  #devolver el data con los NA imputados
+
+}
+
+
+
+# Handling Time Uniqueness ------------------------------------------------
+
+HLTimeUniqueness <- function(data, var_time_name){
+
+  dupl <- duplicated(data[[var_time_name]])
+
+  data <- data[!dupl,]
+
+  return(data)
+
+}
+
+
+
+# Handling Low Range ------------------------------------------------------
+
+HLRange <- function(data){
+
+  #coger los elementos que estan fuera de rango
+
+  #opciones : eliminarlos, imputarlos por la media, imputarlos por max/min permitido segun si sobrepasan o no
+
+  #devolver los datos limpios
+
+}
+
+
+#habria que crear funciones comunes para las metricas de Range, Consistency, Typicality y Moderation
+
+
+
+# Handling Low Consistency ------------------------------------------------
+
+HLConsistency <- function(data){
+
+  #lo mismo que con Range.
+
+}
 
 
 # Handling Low Timeliness -------------------------------------------------
@@ -28,3 +91,24 @@ HLTimeliness <- function(data, var_time_name, maxdif, units){
   #en esos huecos meter tantas fechas como se hayan perdido y asignarle al resto de valores NA
 
 }
+
+
+
+# Handling Low Conformity -------------------------------------------------
+
+HLConformity <- function(data){
+
+  #varias opciones segun el problema que se tenga...
+
+  #intentar cambiar las cosas de formato con un as.formato
+
+  #coger las columnas de dataref y dejar solo esas en data.
+
+
+
+}
+
+
+
+
+
