fd8891453b9daf165c48e8c9a347a809a5c24484
mgomez <mgomez@tekniker.es>
2020-07-09 13:58:26 +0200

    different options are added in convertToDataFrame function (different values of frequency)

--- R/timeseries.R
+++ R/timeseries.R
@@ -1,20 +1,31 @@
 convertToDataFrame <- function(ts){
-
+  #HAY UNA FUNCION EN EL PAQUETE TSBOX QUE HACE ESTOOOOO!!!!
   if(class(ts) != 'ts'){stop('Data must be a ts object')}
 
   df <- as.data.frame(ts)
 
   f <- frequency(ts)
-  start <- c(start(ts),1)
-  end <- c(end(ts), 1)
+  start <- start(ts)
+  end <- end(ts)
 
-  from <- lubridate::ymd(paste(start[1], "-", start[2], "-", start[3]))
-  to <- lubridate::ymd(paste(end[1], "-", end[2], "-", end[3]))
+  if(f==1){
 
-  if(f==12){
+    date<-seq(from, to, by="year")
 
+  }else if(f==12){
+
+    start <- c(start(ts),1)
+    end <- c(end(ts), 1)
+    from <- lubridate::ymd(paste(start[1], "-", start[2], "-", start[3]))
+    to <- lubridate::ymd(paste(end[1], "-", end[2], "-", end[3]))
     date <- seq(from, to, by="month")
 
+  }else if(f==4){
+
+    quarters <- c('01','04','07','10')
+
+  }else{
+    date<-NA
   }
 
   df[['date']] <- date
