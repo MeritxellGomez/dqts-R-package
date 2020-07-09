aae0a9deee1fe05eb3c7fbfaaec1b077b1eee021
mgomez <mgomez@tekniker.es>
2020-07-09 13:58:40 +0200

    the value of each metric is added in the bar plot

--- R/visualization.R
+++ R/visualization.R
@@ -7,11 +7,12 @@ qualitybarplot <- function(DQ, totalquality = FALSE){
 
   df.quality <- data.frame(id=seq(1,length(DQ)),
                            metric = names(DQ),
-                           score = unlist(DQ), stringsAsFactors = FALSE)
+                           score = round(unlist(DQ),3), stringsAsFactors = FALSE)
 
   library(ggplot2)
   g <- ggplot(data=df.quality, aes(x=metric, y=score)) +
     geom_bar(stat="identity", width = 0.4) +
+    geom_text(aes(label=score), vjust = 1.6, size = 3, color="white")+
     # coord_cartesian(xlim = c(NA, NA), ylim = c(65,100)) +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 90, hjust=0.9, vjust = 0.2, size = 15), axis.title.x = element_blank())
