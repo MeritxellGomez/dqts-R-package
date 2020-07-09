e52ddbcdbbe735a18f39179c3560dd9be42b7b6d
mgomez <mgomez@tekniker.es>
2020-07-09 09:55:49 +0200

    date time is modified in convertToDataFrame function. Now a date sequence is created from ts information

--- R/timeseries.R
+++ R/timeseries.R
@@ -3,7 +3,21 @@ convertToDataFrame <- function(ts){
   if(class(ts) != 'ts'){stop('Data must be a ts object')}
 
   df <- as.data.frame(ts)
-  df[['date']] <- as.Date(time(ts))
+
+  f <- frequency(ts)
+  start <- c(start(ts),1)
+  end <- c(end(ts), 1)
+
+  from <- lubridate::ymd(paste(start[1], "-", start[2], "-", start[3]))
+  to <- lubridate::ymd(paste(end[1], "-", end[2], "-", end[3]))
+
+  if(f==12){
+
+    date <- seq(from, to, by="month")
+
+  }
+
+  df[['date']] <- date
 
   return(df)
 
