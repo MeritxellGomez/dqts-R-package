8e3e328028e33c773d63c285858c9782afbb35f5
mgomez <mgomez@tekniker.es>
2020-07-21 13:58:06 +0200

    createTimeFeature function is added

--- R/imputation.R
+++ R/imputation.R
@@ -15,10 +15,8 @@ impmean<- function(var, idna, future = TRUE){
 }
 
 
-
-
 impKNPTS<-function(var, idna, future = TRUE){
-
+  browser()
   npred <- length(idna)
 
   #dejo el crossvalidation para mas tarde... de momento escojo un valor cualquiera de k
@@ -37,4 +35,40 @@ impKNPTS<-function(var, idna, future = TRUE){
 
   return(pred)
 
+}
+
+
+impKNFTS <- function(var, idna, future = TRUE){
+
+  firstna <- min(idna)
+
+  trainset <- data[-idna,]
+  trainset <- createTimeFeatures(trainset, var_time_name)
+
+  #caret::train() ... no tengo idea de como escribir esto cogiendo los nombres de las variables que toca
+
+}
+
+createTimeFeatures <- function(trainset, var_time_name){
+
+  var <- trainset[[var_time_name]]
+
+  trainset[['year']] <- lubridate::year(var)
+  trainset[['month']] <- lubridate::month(var)
+    trainset[['sinmonth']] <- sin(2*pi*trainset[['month']] / 12)
+    trainset[['cosmonth']] <- cos(2*pi*trainset[['month']] / 12)
+  trainset[['day']] <- lubridate::day(var)
+    trainset[['sinday']] <- sin(2*pi*trainset[['day']] / 31)
+    trainset[['cosday']] <- cos(2*pi*trainset[['day']] / 31)
+  trainset[['hour']] <- lubridate::hour(var)
+    trainset[['sinhour']] <- sin(2*pi*trainset[['hour']] / 24)
+    trainset[['coshour']] <- cos(2*pi*trainset[['hour']] / 24)
+  trainset[['minute']] <- lubridate::minute(var)
+    trainset[['sinminute']] <- sin(2*pi*trainset[['minute']] / 60)
+    trainset[['cosminute']] <- cos(2*pi*trainset[['minute']] / 60)
+  trainset[['second']] <- lubridate::second(var)
+    trainset[['sinsecond']] <- sin(2*pi*trainset[['second']] / 60)
+    trainset[['cossecond']] <- cos(2*pi*trainset[['second']] / 60)
+
+  return(trainset)
 }
