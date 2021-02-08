1d7be8879a03b9a14bd3da47cbda129f8f3ef1da
mgomez <mgomez@tekniker.es>
2021-02-08 08:34:59 +0100

    wdwind changes to windowpartition (using caret function and initialWindow, skipo and fixedWindow arguments)

--- R/quality.R
+++ R/quality.R
@@ -17,7 +17,7 @@
 #' @return A data frame containing the values of all quality metrics in columns
 #' @importFrom magrittr %>%
 #' @export
-DQ <- function(data, columnDate = NULL, var_time_name = NULL, maxdif = NULL, units = 'secs', dataref = NULL, ranges = NULL, weights = NULL, windows=FALSE, cte=TRUE, fixed=TRUE, nint=3, by=10){
+DQ <- function(data, columnDate = NULL, var_time_name = NULL, maxdif = NULL, units = 'secs', dataref = NULL, ranges = NULL, weights = NULL, windows=FALSE, initialWindow = 60, skip = 10, fixedWindow = TRUE){
 
   if(class(data) == 'ts'){data <- tsbox::ts_df(data)}
 
@@ -55,7 +55,7 @@ DQ <- function(data, columnDate = NULL, var_time_name = NULL, maxdif = NULL, uni
   if(isFALSE(windows)){
     myquality <- quality(data, columnDate, maxdif, units, dataref, ranges, weights)
   }else{
-    minidf <- data %>% nrow() %>% wdwind(., cte, fixed, nint, by) %>% segwdw(data,.)
+    minidf <- data %>% nrow() %>% windowpartitions(., initialWindow, skip, fixedWindow) %>% segwdw(data,.)
     myquality <- do.call(rbind,lapply(minidf, function(x) quality(x, columnDate, maxdif, units, dataref, ranges, weights)))
   }
 
