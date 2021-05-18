cc9688818673a6831443c3c79618a5df767f34d0
mgomez <mgomez@tekniker.es>
2021-05-18 13:53:45 +0200

    segwdw function added again

--- R/aux_windows.R
+++ R/aux_windows.R
@@ -1,3 +1,19 @@
+segwdw <- function(data, ind){
+
+  n <- nrow(data)
+  l_i <- length(ind)
+
+  dfs <- list()
+
+  for(i in 1:l_i){
+
+    dfs[[i]] <- data[ind[[i]],]
+
+  }
+
+  return(dfs)
+
+}
 
 #windows caret
 
